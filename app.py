dcdcca8a5f1301286969bbe642d325c337609f29
Aditya Shastri <ad.shastri1@gmail.com>
2020-02-05 15:05:36 -0500

    add twilio

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from twilio.rest import Client
 app = Flask(__name__)
+
+client = Client('id', 'token')
 
 @app.route('/one')
 def page_one():
@@ -11,8 +14,10 @@ def page_two():
 
 @app.route('/text_one', methods=["POST"])
 def text_one():
-	pass
+	message = client.messages.create(to="+16093690255", from_="+12013409962", body="You liked a dog!")
+	return "Hey, I like dogs!"
 
 @app.route('/text_two', methods=["POST"])
 def text_two():
-	pass
+	message = client.messages.create(to="+16093690255", from_="+12013409962", body="You liked a cat!")
+	return "Hey, I like cats!"
